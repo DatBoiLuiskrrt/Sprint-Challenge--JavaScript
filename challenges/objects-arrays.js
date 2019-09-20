94f432e73129e1014f9df3e4bd9947d986a0b7d5
Luis Hernandez <datboiluiskrrt@gmail.com>
2019-09-20 11:39:35 -0500

    finished request 1 of array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,50 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyrannosaurus = {
+  diet: "carnivorous",
+  weight: 7000,
+  length: 12,
+  period: "late cretaceous",
+  katyPerry: function(){
+    return "roaaaaaaaaaaaaaaaaaaaaaaaaaaaaaar";
+  }
 
+}
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus = {
+  diet: "herbivorous",
+  weight:2000,
+  length: "9M",
+  period: " Late Jurassic"
 
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const velociraptor = {
+  diet: " carnivorous",
+  weight: "15Kg",
+  length: "1.8m",
+  period: "  Late Cretaceous"
 
+}
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+
+console.log(tyrannosaurus.weight + "Kg");
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.katyPerry());
 
 
 // ==== Arrays ====
@@ -52,7 +74,10 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities);
+graduates.forEach(function(graduates){
+  return universities.push(`${graduates.university}`);
+})
+console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
